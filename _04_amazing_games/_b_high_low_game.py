bbd61c91155959304433281c05d5cce87ab6ea04
League <Buejo2025>
2022-06-23 11:32:37 -0700

    I will finish the coding I worked tomorrow.

--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -7,14 +7,22 @@ if __name__ == '__main__':
     window.withdraw()
 
     # 1. Change this line to give you a random number between 1 - 100.
-    random_num = random.randint(1, 10)
+    random_num = random.randint(1, 100)
 
     # 2. Print out the random variable that you made in step #1
-
+    print(random_num)
     # 3. Code a for loop to run steps 4-10, 10 times
-
+    for i in range(10):
+        Number = simpledialog.askinteger(title='Game4', prompt="1-100")
+        if Number==random_num:
+            messagebox.showinfo(title='Game4', message="You won")
+            sys.exit(0)
+        elif Number > random_num:
+            messagebox.showinfo(title='Game4', message="too high")
+        elif Number < random_num:
+            messagebox.showinfo(title='Game4', message="too low")
         # 4. Ask the user for a guess using a pop-up window, and save their response
-
+    messagebox.showinfo(title='Game4', message="You lost")
         # 5. If the guess is correct
             # 6. Win. Use 'sys.exit(0)' to end the program
 
